5ca7edd8fd576f0a92e721d948b0a07b106f1288
tanujn45 <tanujn45@gmail.com>
2020-10-10 13:26:00 +0530

    hemant sucks

--- Navigators/SubProgramNavigator.js
+++ Navigators/SubProgramNavigator.js
@@ -1,4 +1,4 @@
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 const Tab = createMaterialTopTabNavigator();
 
