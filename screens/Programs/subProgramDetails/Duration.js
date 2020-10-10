c270f3248d0ad26d34f77ef170ff04f5548130bc
hemantjangid <hemantjangid.gyrix@gmail.com>
2020-10-10 14:09:28 +0530

    Eligiblity and duration

--- screens/Programs/subProgramDetails/Duration.js
+++ screens/Programs/subProgramDetails/Duration.js
@@ -1,14 +1,94 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 
 const Duration = (props) => {
+  const duration = [
+    {
+      key: "1",
+      point: "The duration of the program is two years.",
+    },
+    {
+      key: "2",
+      point: "The registration period is valid for four academic years.",
+    },
+  ];
+  const exit = [
+    {
+      key: "1",
+      point:
+        "Only PGDBA students who complete one full year of study (I and II Semesters) and successfully pass the examination and assignment corresponding to first two semesters, but who are unable to complete the III and IV Semesters due to unavoidable circumstances, will be eligible for a Diploma in Management (DIM).",
+    },
+    {
+      key: "2",
+      point:
+        "An eligible student will have to make a specific application to SCDL to opt out of the PGDBA Program within the registration validity period of PGDBA. The fees paid for PGDBA are non-refundable and non-transferable.",
+    },
+  ];
   return (
-    <View>
-      <Text>Duration</Text>
+    <View style={styles.content}>
+      <FlatList
+        data={duration}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.points}>
+              <Feather
+                name="star"
+                size={15}
+                color="black"
+                style={{ marginTop: 10 }}
+              />
+              <Text style={styles.point}>{item.point}</Text>
+            </View>
+          );
+        }}
+      ></FlatList>
+      <View style={styles.exitContent}>
+        <Text style={styles.exit}>Exit Policy :</Text>
+        <FlatList
+          data={exit}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.points}>
+                <Feather
+                  name="star"
+                  size={15}
+                  color="black"
+                  style={{ marginTop: 10 }}
+                />
+                <Text style={styles.point}>{item.point}</Text>
+              </View>
+            );
+          }}
+        ></FlatList>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    width: "90%",
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  points: {
+    flexDirection: "row",
+    marginTop: 10,
+    alignItems: "flex-start",
+  },
+  point: {
+    marginLeft: 10,
+    fontSize: 15,
+    width: "90%",
+  },
+  exitContent: {
+    marginTop: 20,
+  },
+  exit: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
 
 export default Duration;
--- screens/Programs/subProgramDetails/Eligiblity.js
+++ screens/Programs/subProgramDetails/Eligiblity.js
@@ -1,14 +1,64 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const data = [
+  {
+    key: "1",
+    point:
+      "Bachelor's degree holder / Graduate in any discipline from a recognised University.",
+  },
+  {
+    key: "2",
+    point:
+      "International / SAARC Graduate from a recognised / accredited University / Institution.",
+  },
+  {
+    key: "3",
+    point:
+      "Students who have appeared for final year of examination of their bachelor's degree program and are awaiting results can also apply, subject to successfully completing their bachelor's degree program / graduation within the time period specified by SCDL.",
+  },
+];
 
 const Eligiblity = (props) => {
   return (
-    <View>
-      <Text>Eligiblity</Text>
+    <View style={styles.content}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.points}>
+              <Feather
+                name="star"
+                size={15}
+                color="black"
+                style={{ marginTop: 10 }}
+              />
+              <Text style={styles.point}>{item.point}</Text>
+            </View>
+          );
+        }}
+      ></FlatList>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    width: "90%",
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  points: {
+    flexDirection: "row",
+    marginTop: 10,
+    alignItems: "flex-start",
+  },
+  point: {
+    marginLeft: 10,
+    fontSize: 15,
+    width: "90%",
+  },
+});
 
 export default Eligiblity;
